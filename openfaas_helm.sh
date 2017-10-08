bc4d83709e944a286adbf98a7ede07c3f6372aac
Jock Reed <jock.da.rock@gmail.com>
2017-10-08 02:08:50 -0500

    Update openfaas_helm.sh

--- openfaas_helm.sh
+++ openfaas_helm.sh
@@ -25,6 +25,6 @@ helm init --skip-refresh --upgrade --service-account tiller
 echo "Installing OpenFaaS using the OpenFaaS Helm Chart"
 helm upgrade --install --debug --reset-values --set async=true openfaas openfaas
 
-sleep 10
 echo "Installing sample functions for OpenFaaS"
+sleep 10
 faas-cli deploy -f https://raw.githubusercontent.com/openfaas/faas-cli/master/samples.yml --gateway http://127.0.0.1:31112
